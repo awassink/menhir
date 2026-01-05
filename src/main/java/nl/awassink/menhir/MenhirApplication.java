728799101c360219eb938015b0845f6e9b612a90
Arjen Wassink <awassink@quintor.nl>
2026-01-05 11:40:55 +0100

    initial setup

---
+++ src/main/java/nl/awassink/menhir/MenhirApplication.java
@@ -0,0 +1,13 @@
+package nl.awassink.menhir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenhirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenhirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/awassink/menhir/MenhirApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.awassink.menhir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenhirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
